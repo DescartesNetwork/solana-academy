1169f36c5552c4457ac8b2e8aa7bbb8cbb761c79
dmcong <83624415+dmcong@users.noreply.github.com>
2022-03-28 14:07:50 +0700

    update webpack

--- craco.config.js
+++ craco.config.js
@@ -1,25 +1,11 @@
-require("dotenv-cra").config(); // https://github.com/gsoft-inc/craco/issues/180
+require("dotenv-cra").config();
 
-const { CracoLessPlugin } = require("./plugins/craco-less");
 const CracoWasm = require("./plugins/craco-wasm");
 const CracoSilence = require("./plugins/craco-silence");
 const CracoCompatibility = require("./plugins/craco-compatibility");
 
 module.exports = {
   plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-            modifyVars: {
-              "@ant-prefix": "kylan",
-            },
-          },
-        },
-      },
-    },
     {
       plugin: CracoCompatibility,
     },
--- plugins/craco-less/index.js
+++
@@ -1,5 +0,0 @@
-const CracoLessPlugin = require('./index.default')
-
-module.exports = {
-  CracoLessPlugin,
-}
