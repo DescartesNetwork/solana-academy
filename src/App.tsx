75a08b8b791952d41df17ae8bf8bbfed4ebe1e41
dmcong <83624415+dmcong@users.noreply.github.com>
2022-04-05 22:32:08 +0700

    create react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,13 @@
-import { useCallback, useEffect, useState } from "react";
-import { useWalletKit, useSolana, useConnectedWallet } from "@gokiprotocol/walletkit";
+import { Col, Row } from 'antd'
 
-import { Button, Col, Row } from "antd";
-import WalletInfo from "components/walletInfo";
-
-import "./App.css";
+import './App.css'
 
 function App() {
-  // State: balance (type = number, default value = 0)
-  const [balance, setBalance] = useState<number>(0);
-  // Goki hooks
-  const wallet = useConnectedWallet();
-  const { connect } = useWalletKit();
-  const { disconnect, providerMut } = useSolana();
-
-  const fetchBalance = useCallback(async () => {
-    // TODO: fetch balance
-    if (wallet && providerMut) {
-      let balance = await providerMut.connection.getBalance(wallet.publicKey);
-      return setBalance(balance);
-    }
-    setBalance(0);
-  }, [providerMut, wallet]);
-
-  useEffect(() => {
-    fetchBalance();
-  }, [fetchBalance]);
-
   return (
     <Row justify="center">
-      <Col>
-        <WalletInfo address={wallet?.publicKey.toBase58() || ""} balance={balance} />
-      </Col>
-      {/* Button connect wallet */}
-      <Col span={24} style={{ textAlign: "center" }}>
-        {wallet ? (
-          <Button type="primary" onClick={disconnect}>
-            Disconnect
-          </Button>
-        ) : (
-          // Call connectWallet function when click Button
-          <Button type="primary" onClick={connect}>
-            Connect Wallet
-          </Button>
-        )}
-      </Col>
+      <Col>My App</Col>
     </Row>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { WalletKitProvider } from "@gokiprotocol/walletkit";
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <WalletKitProvider
-      defaultNetwork="devnet"
-      app={{
-        name: "My App",
-      }}
-    >
-      <App />
-    </WalletKitProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
